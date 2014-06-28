001b2197a425421998bf2abb901af40f23b89058
Jakob Westhoff <jakob@westhoffswelt.de>
2014-06-28 14:49:20 +0200

    Implemented magic promise wrapping

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-exports.L8 = require("./Library/L8").L8;
+var Promisify = require("./Library/Promisify").Promisify;
+
 exports.SLCP = require("./Library/SLCP");
+
+exports.L8 = require("./Library/L8").L8;
+Promisify(exports.L8.prototype);
