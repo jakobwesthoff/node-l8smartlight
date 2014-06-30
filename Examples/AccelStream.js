57b78a38e49acfdf431b3cc8941913f5687dbd82
Tobias Struckmeier <tos@crosscan.com>
2014-07-01 01:19:16 +0200

    Cleaned up example

--- Examples/AccelStream.js
+++ Examples/AccelStream.js
@@ -4,14 +4,6 @@ var SERIAL_PORT = "/dev/tty.L8-SerialPortServerPort1";
 
 var l8 = new L8();
 
-
-l8.on("frameReceived", function(frame) {
-    console.log("RECEIVED: ", frame.payload.toString("hex"));
-});
-l8.on("frameSent", function(frame) {
-    console.log("SENT: ", frame.toString("hex"));
-});
-
 l8.open(SERIAL_PORT, null, function(error, response) {
     if (error) {
         throw new Error(error);
@@ -19,13 +11,12 @@ l8.open(SERIAL_PORT, null, function(error, response) {
 
     l8.setOrientation('up');
     l8.clearMatrix();
-    //l8.setScrollingText('Move me baby!', {r: 0, g: 15, b: 0}, "fast", true);
+    l8.setScrollingText('Move me baby!', {r: 0, g: 15, b: 0}, "fast", true);
 
     var stream = l8.getAccelerationStream(100);
 
     stream.on('data', function(data) {
         console.log(data);
     });
-
 
 });
