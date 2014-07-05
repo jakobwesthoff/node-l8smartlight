dea0fb2907e4b75eaa48c5e9198f3b6d798097ed
Jakob Westhoff <jakob@westhoffswelt.de>
2014-07-05 13:14:21 +0200

    Cleaned up `AccelerationStream` example

---
+++ Examples/AccelerationStream.js
@@ -0,0 +1,22 @@
+var L8 = require("../index").L8;
+
+var SERIAL_PORT = "/dev/tty.L8-SerialPortServerPort1";
+
+var l8 = new L8();
+
+l8.open(SERIAL_PORT, null).then(function() {
+    return l8.setOrientation('up');
+}).then(function() {
+    l8.clearMatrix();
+}).then(function() {
+    l8.setScrollingText('Move me!', {r: 0, g: 15, b: 0}, "fast", true);
+}).then(function() {
+    var acceleration = l8.createAccelerationStream(500);
+
+    acceleration.on('data', function(data) {
+        console.log(data);
+    });
+}).catch(function(error) {
+    console.error(error);
+});
+
