d7aa0be1941e40181be337658715d74a9200aff8
Jakob Westhoff <jakob@westhoffswelt.de>
2014-07-05 12:47:44 +0200

    Export `AccelerationStream`

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ exports.SLCP = require("./Library/SLCP");
 
 // Helper to build LED matrices
 exports.MatrixBuilder = require("./Library/MatrixBuilder").MatrixBuilder;
+// Stream to continuously access accelerometer data.
+exports.AccelerationStream = require("./Library/AccelerationStream").AccelerationStream;
 
 // Main entry point to control a L8
 exports.L8 = require("./Library/L8").L8;
