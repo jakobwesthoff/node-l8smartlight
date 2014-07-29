0e0993db2efaede9f0c352d34ba65060b67a65f0
Jakob Westhoff <jakob@westhoffswelt.de>
2014-07-29 23:06:19 +0200

    Added basic Grid Test

--- Examples/GroupedL8Test.js
+++ Examples/GroupedL8Test.js
@@ -5,14 +5,14 @@ var GridBuilder = require("../Library/GridBuilder").GridBuilder;
 var L8Grid = require("../Library/L8Grid").L8Grid;
 
 var SERIAL_PORTS = [
-    "/dev/tty.usbmodem1d11421", /* bottom right */
-    "/dev/tty.usbmodem1d1111", /* bottom left */
-    "/dev/tty.usbmodem1d1131", /* top left */
-    "/dev/tty.usbmodem1d11441" /* top right */
+    "/dev/tty.usbmodem1d1131", /* 0 */
+    "/dev/tty.usbmodem1d1111", /* 1 */
+    "/dev/tty.usbmodem1d11441", /* 2 */
+    "/dev/tty.usbmodem1d11421" /* 3 */
 ];
 
-var connections = SERIAL_PORTS.map(function (SERIAL_PORT, index) {
-    var l8 = new L8();
+var l8s = SERIAL_PORTS.map(function (SERIAL_PORT, index) {
+    var l8 = new L8(SERIAL_PORT);
 
     l8.on("frameReceived", function (frame) {
         console.log("[" + index + "] RECEIVED: ", frame.payload.toString("hex"));
@@ -21,59 +21,42 @@ var connections = SERIAL_PORTS.map(function (SERIAL_PORT, index) {
         console.log("[" + index + "] SENT: ", frame.toString("hex"));
     });
 
-    return {
-        l8: l8,
-        promise: l8.open(SERIAL_PORT, null)
-    };
+    return l8;
 });
 
-var l8grid = null;
+var l8grid = new L8Grid(
+    new GridBuilder(l8s[0])
+     .right(l8s[1])
+     .right(l8s[2])
+     .right(l8s[3])
+     .toGrid()
+);
 
-RSVP.all(connections.map(function(connection) {
-    return connection.promise;
-})).then(function() {
-    console.log("CONNECTED TO ALL!");
+l8grid.open(function(error) {
+    l8grid.clearMatrix(function() {
+        var x, y;
+        var timeout = 0;
+        var colors = [
+            {r: 15, g: 15, b: 0},
+            {r: 15, g: 0, b: 0},
+            {r: 0, g: 0, b: 15},
+            {r: 0, g: 15, b: 0},
+            {r: 0, g: 15, b: 15},
+            {r: 0, g: 0, b: 0}
+        ];
+        var colorIndex = 0;
 
-/*    var grid = new GridBuilder(connections[0].l8)
-        .left(connections[1].l8)
-        .top(connections[2].l8)
-        .right(connections[3].l8)
-        .toGrid();
-*/
-    var grid = new GridBuilder(connections[2].l8)
-     .right(connections[3].l8)
-     .bottom(connections[0].l8)
-     .left(connections[1].l8)
-     .toGrid();
-
-    l8grid = new L8Grid(grid);
-    return l8grid.clearGrid();
-}).then(function() {
-    var x, y;
-    var timeout = 0;
-    var colors = [
-        {r: 15, g: 15, b: 0},
-        {r: 15, g: 0, b: 0},
-        {r: 0, g: 0, b: 15},
-        {r: 0, g: 15, b: 0},
-        {r: 0, g: 15, b: 15},
-        {r: 0, g: 0, b: 0}
-    ];
-    var colorIndex = 0;
-
-    for(colorIndex = 0; colorIndex < colors.length; colorIndex++) {
-        for(y=0; y<16; y++) {
-            for(x=0; x<16; x++) {
-                (function(x, y, timeout, colorIndex) {
-                    setTimeout(function() {
-                        l8grid.setLED(x, y, colors[colorIndex], function() {});
-                    }, timeout);
-                }(x, y, timeout, colorIndex));
-                timeout += 10;
+        for(colorIndex = 0; colorIndex < colors.length; colorIndex++) {
+            for(x=0; x<32; x++) {
+                for(y=0; y<8; y++) {
+                    (function(x, y, timeout, colorIndex) {
+                        setTimeout(function() {
+                            l8grid.setLED(x, y, colors[colorIndex], function() {});
+                        }, timeout);
+                    }(x, y, timeout, colorIndex));
+                    timeout += 10;
+                }
             }
         }
-    }
-}).catch(function(error) {
-   console.error(error);
+    });
 });
-
